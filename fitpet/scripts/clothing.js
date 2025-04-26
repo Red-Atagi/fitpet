1ea082a4fb02fd5da6889da451324c8d399ff238
BMoore03 <brandinm@uchicago.edu>
2025-04-25 19:10:39 -0500

    Skeleton clothing files

--- fitpet/scripts/clothing.js
+++ fitpet/scripts/clothing.js
@@ -1,5 +1,27 @@
 class Clothing {
-    constructor(id, type, price, image_path) {
+    #clothingID; // private
+    #clothingPrice;
+    #clothingType;
+    constructor(image_path) {
         TODO
+        this.image_path = image_path;
+    }
+
+
+    getPrice() {
+        return this.#clothingPrice;
+    }
+
+
+    getClothingId() {
+        return this.#clothingID;
+    }
+
+
+    getClothingType() {
+        return this.#clothingType;
     }
 }
+
+const pants = Clothing()
+
---
+++ fitpet/scripts/tests/clothing.test.js
@@ -0,0 +1,5 @@
+const getPrice = require('./clothing');
+
+test('Get the price of an item', () => {
+  expect(sum(1, 2)).toBe(3);
+});
