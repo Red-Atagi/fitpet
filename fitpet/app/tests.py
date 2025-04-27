86a56e29e0b298010e0f70972ca5c4659670b184
Kaitlyn Li <kaitlynli2025@uchicago.edu>
2025-04-27 17:30:30 -0500

    tests setup

--- fitpet/app/tests.py
+++ fitpet/app/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+# from django.urls import reverse
+from app.models import *
 
-# Create your tests here.
+class CreateAcountTestCase(TestCase):
+    def SetUp(self):
+        ...
+    
+    def test_1(self):
+        ...
+
+class ShopTestCase(TestCase):
+    def SetUp(self):
+        ...
+    
+    def test_1(self):
+        ...
+
+class DressPetTestCase(TestCase):
+    def SetUp(self):
+        ...
+    
+    def test_1(self):
+        ...
+
+class WorkoutTestCase(TestCase):
+    def SetUp(self):
+        ...
+    
+    def test_1(self):
+        ...
