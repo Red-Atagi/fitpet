a47577a519fce73a82c1fb2563ae060771bb1a56
BMoore03 <brandinm@uchicago.edu>
2025-05-05 13:57:02 -0500

    Changed shop item modal

--- fitpet/app/urls.py
+++ fitpet/app/urls.py
@@ -6,12 +6,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('shop/', views.shop_page, name='shop_page'),
-    path('dress/', views.display_dress_page, name='dress_page'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('dress/update_clothing/', views.update_clothing, name='update_clothing'),
-    path('api/buyclothing/', views.buy_clothing, name='buy_clothing'),
+    path('shop', views.shop_page, name='shop_page'),
+    path('dress', views.display_dress_page, name='dress_page'),
+    path('accounts/login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('dress/update_clothing', views.update_clothing, name='update_clothing'),
+    path('api/buyclothing', views.buy_clothing, name='buy_clothing'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
